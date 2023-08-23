fb337ff39b4ac832f126e3afef8affd2077402af
Mayla <mayumi.mayla@gmail.com>
2023-08-23 12:56:53 -0300

    criando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CabecalhoComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
