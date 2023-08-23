1d458c89b7fc926569d419e5bdff0d1c94196379
Vitor Vargem <tp.vitor.vargem>
2023-08-23 13:44:43 -0300

    Footer Route add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { ContatoComponent } from './contato/contato.component';
+import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
   { path: 'cursos', component: CursosComponent},
-  { path: 'contato', component: ContatoComponent}
+  { path: 'contato', component: ContatoComponent},
+  { path: 'footer', component: FooterComponent},
 ];
 
 @NgModule({
