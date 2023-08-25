8390ec183ce917bc3bdc6d395bee89d5cdd26028
Vitor Vargem <tp.vitor.vargem>
2023-08-24 23:08:50 -0300

    App.module.ts bug soluction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,16 +17,9 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {MatGridListModule} from '@angular/material/grid-list';
-<<<<<<< HEAD
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-=======
 import {MatDialogModule} from '@angular/material/dialog';
 import { LiveFormDialogComponent } from './live-form-dialog/live-form-dialog.component';
-
-
-
-
->>>>>>> adf62851cb805577e25af36033c896ea61aaedac
 
 @NgModule({
   declarations: [
@@ -47,12 +40,9 @@ import { LiveFormDialogComponent } from './live-form-dialog/live-form-dialog.com
     MatIconModule,
     MatToolbarModule,
     MatGridListModule,
-<<<<<<< HEAD
-    NgFor
-=======
+    NgFor,
     MatDialogModule
 
->>>>>>> adf62851cb805577e25af36033c896ea61aaedac
   ],
   providers:[],
   bootstrap: [AppComponent]
