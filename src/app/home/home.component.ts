4195ae54b9f625f67a0320247a1b58bb28e317bd
FelipeLRomao <feliperomaodev@gmail.com>
2023-08-25 16:44:28 -0300

    adicionando ngFor para lista de json cursos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -77,5 +77,104 @@ export class HomeComponent implements OnInit, OnDestroy {
        this.iniciarTimer();
      }
 
+    //  json cursos
+    cursos:Array<any> = [
 
-}
+      {
+        id: 1,
+        name: "Python",
+        description: "Começando com a linguagem",
+        foto:"https://i.ibb.co/dLCkbFw/python.jpg",
+        preco: "159.80",
+        favorito: true
+      },
+      {
+        id: 2,
+        name: "Javascript para Web",
+        description: "Crie páginas dinâmicas",
+        foto: "https://i.ibb.co/jT3Bw8y/java-Script.jpg",
+        preco: "220.79",
+        favorito: true
+      },
+      {
+        id: 3,
+        name: "Angular",
+        description: "Componentização e design com Angular Material",
+        foto:"https://i.ibb.co/ky1kCL3/angular.jpg",
+        preco: "275.20",
+        favorito: true
+      },
+      {
+        id: 4,
+        name: "Inteligencia Artificial e UX",
+        description: "Otimize a construção de um produto digital",
+        foto: "https://i.ibb.co/n0BWBbw/artificial.jpg",
+        preco: "280.15",
+        favorito: true
+      },
+      {
+        id: 5,
+        name: "Mobile: IOS e HTTP",
+        description: "Web Services e sicronização offine",
+        foto: "https://i.ibb.co/Xj93ZLz/mobile.jpg",
+        preco: "195.49",
+        favorito: false
+      },
+      {
+        id: 6,
+        name: "HTML e CSS",
+        description: "Praticando HTML/CSS",
+        foto: "https://i.ibb.co/0c5Z330/html-css.webp",
+        preco: "150.00",
+        favorito: false
+      },
+      {
+        id: 7,
+        name: "SQL com MySQL",
+        description: "Manipule e consulte dados",
+        foto: "https://i.ibb.co/b7bbd7F/sql-mysql1.jpg",
+        preco: "184.99",
+        favorito: false
+      },
+      {
+        id: 8,
+        name: "UX Usability",
+        description: "Facilite a vida do seu usuário no mobile",
+        foto: "https://i.ibb.co/hVqgdSz/ux-usability.jpg",
+        preco: "165.55",
+        favorito: false
+      },
+      {
+        id: 9,
+        name: "Data Science",
+        description: "Visualização de dados para saúde e medicina",
+        foto: "https://i.ibb.co/dt4X5Gs/data-science.jpg",
+        preco: "160.77",
+        favorito: false
+      },
+      {
+        id: 10,
+        name: "Jogos Digitais",
+        description: "Criação de programas e ambientes de jogos",
+        foto: "https://i.ibb.co/QXcXHwn/desenvolvimento-de-jogos.png",
+        preco: "230.49",
+        favorito: false
+      },
+      {
+        id: 11,
+        name: "React",
+        description: "lidando com arquivos estáticos",
+        foto: "https://i.ibb.co/s96b5tw/react.webp",
+        preco: "185.30",
+        favorito: false
+      },
+      {
+        id: 12,
+        name: "Linux I",
+        description: "conhecendo e utilizando o terminal",
+        foto: "https://i.ibb.co/gFqfNZ8/linux.webp",
+        preco: "275.00",
+        favorito: false
+      }
+    ]
+  }
