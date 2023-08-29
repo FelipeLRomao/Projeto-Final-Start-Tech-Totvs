0245a778bd7ac8b9ea221ade06251ab57a323f15
Vitor Vargem <tp.vitor.vargem>
2023-08-28 21:27:47 -0300

    Delete confirm dialog / json-data-base copy / create authentication function / create user function

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,20 +1,24 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+import { MatDialog, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
-import { MatSnackBarModule, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition, MatSnackBar} from '@angular/material/snack-bar';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatSnackBarModule, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition, MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss'],
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatIconModule, MatSnackBarModule],
+  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatIconModule, MatSnackBarModule, MatDialogModule],
 })
 
-export class AdminComponent implements OnInit{
+export class AdminComponent implements OnInit {
   cursos: any;
   horizontalPosition: MatSnackBarHorizontalPosition = 'end';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
@@ -22,10 +26,11 @@ export class AdminComponent implements OnInit{
   displayedColumns: string[] = ['foto', 'descricao', 'acoes'];
   dataSource = new MatTableDataSource();
 
-  constructor (
+  constructor(
     private _snackBar: MatSnackBar,
-    private http: HttpClient
-  ) {}
+    private http: HttpClient,
+    public dialog: MatDialog,
+  ) { }
 
   ngOnInit(): void {
     this.listarCurso();
@@ -45,6 +50,14 @@ export class AdminComponent implements OnInit{
 
   }
 
+  openDeletarCurso(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(ExcludeConfirm, {
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
+
   deletarCurso(cursosId: string): void {
     this.http.delete('http://localhost:3000/cursos/' + cursosId).subscribe(response => {
       this._snackBar.open('O curso foi removido!', 'Fechar', {
@@ -54,25 +67,23 @@ export class AdminComponent implements OnInit{
       });
       this.listarCurso();
     },
-    error => {
-      this._snackBar.open('Ocorreu um erro ao remover o curso ' + error, 'Fechar', {
-        horizontalPosition: this.horizontalPosition,
-        verticalPosition: this.verticalPosition,
-        duration: 5000
+      error => {
+        this._snackBar.open('Ocorreu um erro ao remover o curso ' + error, 'Fechar', {
+          horizontalPosition: this.horizontalPosition,
+          verticalPosition: this.verticalPosition,
+          duration: 5000
+        });
       });
-    });
   }
 
   toggleFavorito(cursosId: string): void {
     this.http.get<any>('http://localhost:3000/imoveis/' + cursosId).subscribe(data => {
 
       this.cursos = data;
-
       this.cursos.favorito = !this.cursos.favorito;
       this.http.patch('http://localhost:3000/cursos/' + cursosId, { favorito: this.cursos.favorito })
         .subscribe(
           response => {
-            // console.log('Property favorito status updated successfully:', response);
             if (this.cursos.favorito === true) {
               this._snackBar.open('O curso foi favoritado!', 'Fechar', {
                 horizontalPosition: this.horizontalPosition,
@@ -92,24 +103,81 @@ export class AdminComponent implements OnInit{
             });
           },
           error => {
-            // console.error('Error updating property favorito status:', error);
-              this._snackBar.open('Ocorreu um erro ao favoritar/desfavoritar o curso!', 'Fechar', {
-                horizontalPosition: this.horizontalPosition,
-                verticalPosition: this.verticalPosition,
-                duration: 5000
-              });
-              // Revert the 'favorito' value if the update fails
-              this.cursos.favorito = !this.cursos.favorito;
-            }
+            this._snackBar.open('Ocorreu um erro ao favoritar/desfavoritar o curso!', 'Fechar', {
+              horizontalPosition: this.horizontalPosition,
+              verticalPosition: this.verticalPosition,
+              duration: 5000
+            });
+            // Revert the 'favorito' value if the update fails
+            this.cursos.favorito = !this.cursos.favorito;
+          }
 
-      );
+        );
     });
-
   }
 
   filtrarCurso(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+}
+
+@Component({
+  selector: 'dialog-excluir',
+  styleUrls: ['dialogExcluir.scss'],
+  templateUrl: 'dialogExcluir.html',
+  standalone: true,
+  imports: [MatButtonModule, MatDialogModule],
+})
+export class ExcludeConfirm implements OnInit {
+  curso: any = {}
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+
+  dataSource = new MatTableDataSource();
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private http: HttpClient,
+    private _snackBar: MatSnackBar,
+    public dialogRef: MatDialogRef<ExcludeConfirm>
+  ) { }
+
+  ngOnInit(): void {
+    this.http.get<any>('http://localhost:3000/cursos' + this.data)
+      .subscribe(
+        response => { this.curso = response }
+      )
+  }
+
+  listarCurso(): void {
+    this.http.get<any>('http://localhost:3000/cursos').subscribe(data => {
+      this.dataSource.data = data;
+    });
+  }
+
+  deletarCurso(cursosId: string): void {
+    this.http.get('http://localhost:3000/cursos/' + cursosId)
+      .subscribe(response => {
+        this.http.delete('http://localhost:3000/cursos/' + cursosId)
+        this._snackBar.open('O curso foi removido!', 'Fechar', {
+          horizontalPosition: this.horizontalPosition,
+          verticalPosition: this.verticalPosition,
+          duration: 5000
+        });
+        this.http.get<any>('http://localhost:3000/cursos/').subscribe(data => {
+          this.dataSource.data = data
+        });
+        this.listarCurso();
+        },
+        error => {
+          this._snackBar.open('Ocorreu um erro ao remover o curso ' + error, 'Fechar', {
+            horizontalPosition: this.horizontalPosition,
+            verticalPosition: this.verticalPosition,
+            duration: 5000
+          });
+        });
   }
 }
 
